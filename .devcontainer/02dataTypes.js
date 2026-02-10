e94c8d1ff9a353261569cd3d5c24e1d61eecae73
Dinesh kumar <dy9785409@gmail.com>
2026-02-10 18:14:46 +0000

    container config done

---
+++ .devcontainer/02dataTypes.js
@@ -0,0 +1,16 @@
+//alert(3+2) we are using nodejs , not browser
+ console.log(3+3)
+console.log("Dinesh")
+let name="Dinesh "
+let age=18
+let isloggedIn=true
+console.log(name)
+console.log(age)
+console.log(isloggedIn)
+//number=>2 to power 53
+//bigint
+//String=""
+//boolean=>true/false 
+//undefined =>value is not assigned 
+//symbol=>unique 
+console.log(typeof name)
